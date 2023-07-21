a4457eec1295149eeda86d61ef9b273df3686e1c
ibra-kdbra <git-repo-develop@outlook.com>
2023-07-21 23:30:44 +0300

    added multiple file extraction with comment to explain

--- Extraction.py
+++ Extraction.py
@@ -1,40 +1,66 @@
 import buckeye
-# speaker = buckeye.Speaker.from_zip('C:\\Users\Hadi\\Downloads\\s01.zip', load_wavs=True)
-speaker = buckeye.Speaker.from_zip('s01.zip', load_wavs=True)
-print(speaker.name)
-print(speaker.sex) # f for female, m for male
-print(speaker.age) # o for old, y for young
-print(speaker.interviewer) #
-for track in speaker:
-    print(track.name)
-print(speaker.tracks)
-track = speaker[0]
-print(track)
-print(track.words[:10])
-word = track.words[4]
-print(word.orthography)
-print(word.beg)
-print(word.end)
-print(word.dur)
-print(word.phonemic)
-print(word.phonetic)
-print(word.pos)
-print(word.misaligned)
-for phone in word.phones:
-    print(phone.seg, phone.beg, phone.end, phone.dur)
-pause = track.words[1]
-print(pause.entry, pause.beg, pause.end, pause.dur, pause.misaligned)
-print(pause.phones)
-for phone in track.phones[:10]:
-    print(phone.seg, phone.beg, phone.end, phone.dur)
-for log in track.log:
-    print(log.entry, log.beg, log.end, log.dur)
-logs = track.get_logs(60.0, 62.0)
 
-for log in logs:
-    print(log.entry, log.beg, log.end)
-print(track.txt[1])
-# speaker = buckeye.Speaker.from_zip('speakers/s01.zip', load_wavs=True)
-speaker = buckeye.Speaker.from_zip('s01.zip', load_wavs=True)
-track = speaker[0]
-print(track.wav)
+# Open a file for writing and write speaker and track information to it
+with open('extraction.txt', 'w') as outfile:
+    # Loop through each speaker in the Buckeye dataset (speakers 1-40)
+    for speaker_num in range(1, 41):
+        # Load the Buckeye dataset for the current speaker
+        zip_filename = f's{speaker_num:02d}.zip'
+        speaker = buckeye.Speaker.from_zip(zip_filename, load_wavs=True)
+
+        # Write speaker information to the output file
+        outfile.write(f"Speaker {speaker_num}\n")
+        outfile.write(f"Name: {speaker.name}\n")
+        outfile.write(f"Sex: {speaker.sex}\n")
+        outfile.write(f"Age: {speaker.age}\n")
+        outfile.write(f"Interviewer: {speaker.interviewer}\n")
+
+        # Loop through each track in the current speaker's dataset
+        for track in speaker:
+            # Write track information to the output file
+            outfile.write(f"Track: {track.name}\n")
+        outfile.write(f"Tracks: {speaker.tracks}\n")
+        track = speaker[0]
+        outfile.write(f"Track: {track}\n")
+        outfile.write(f"Words: {track.words[:10]}\n")
+
+        # Get information about a specific word in the current track
+        word = track.words[4]
+        outfile.write(f"Orthography: {word.orthography}\n")
+        outfile.write(f"Beg: {word.beg}\n")
+        outfile.write(f"End: {word.end}\n")
+        outfile.write(f"Dur: {word.dur}\n")
+        outfile.write(f"Phonemic: {word.phonemic}\n")
+        outfile.write(f"Phonetic: {word.phonetic}\n")
+        outfile.write(f"Pos: {word.pos}\n")
+        outfile.write(f"Misaligned: {word.misaligned}\n")
+
+        # Loop through each phone in the current word and write phone information to the output file
+        for phone in word.phones:
+            outfile.write(f"Phone: {phone.seg} {phone.beg} {phone.end} {phone.dur}\n")
+
+        # Get information about a pause in the current track
+        pause = track.words[1]
+        outfile.write(f"Pause: {pause.entry} {pause.beg} {pause.end} {pause.dur} {pause.misaligned}\n")
+        outfile.write(f"Pause phones: {pause.phones}\n")
+
+        # Loop through each phone in the current track and write phone information to the output file
+        for phone in track.phones[:10]:
+            outfile.write(f"Phone: {phone.seg} {phone.beg} {phone.end} {phone.dur}\n")
+
+        # Loop through each log entry in the current track and write log information to the output file
+        for log in track.log:
+            outfile.write(f"Log entry: {log.entry} {log.beg} {log.end} {log.dur}\n")
+
+        # Get log entries within a specific time range in the current track and write them to the output file
+        logs = track.get_logs(60.0, 62.0)
+        for log in logs:
+            outfile.write(f"Log entry: {log.entry} {log.beg} {log.end}\n")
+
+        # Write the text of the current track to the output file
+        outfile.write(f"Text: {track.txt[1]}\n")
+
+        # Write the filename of the current track's WAV file to the output file
+        speaker = buckeye.Speaker.from_zip(zip_filename, load_wavs=True)
+        track = speaker[0]
+        outfile.write(f"Wav: {track.wav}\n")
