9f84f91f73c66b54258d434c04022b1130fd2ad6
ibra-kdbra <git-repo-develop@outlook.com>
2023-08-13 23:53:58 +0300

    custom voice use

---
+++ frontend/src/hooks/useVoice.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { VOICE_KEYS } from '../constants/index';
+import { setVoice } from '../redux/actions/voiceAction';
+
+// get custom voice to store into redux
+function useVoice() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const customVoice = localStorage.getItem(VOICE_KEYS.LS_KEY);
+    if (customVoice) {
+      dispatch(setVoice(JSON.parse(customVoice)));
+    }
+
+  });
+
+  return null;
+}
+
+export default useVoice;
