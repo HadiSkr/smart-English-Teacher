78327f57da348cae889fbfeb626e0e256f0def27
ibra-kdbra <git-repo-develop@outlook.com>
2023-08-14 00:14:58 +0300

    message set and get

---
+++ frontend/src/redux/actions/messageAction.js
@@ -0,0 +1,18 @@
+import MESSAGE_CONSTANT from '../constants/messageConstant';
+
+export const setMessage = (message, type)  => {
+  return {
+    type: MESSAGE_CONSTANT.SET_MESSAGE,
+    payload: {
+      message,
+      type
+    }
+  }
+}
+
+export const closeMessage = () => {
+  return {
+    type: MESSAGE_CONSTANT.CLOSE_MESSAGE
+  }
+}
+
