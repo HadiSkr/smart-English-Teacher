8095d920c230a0271e9dcb5c9e511e741d459e55
ibra-kdbra <git-repo-develop@outlook.com>
2023-08-16 21:16:18 +0300

    gallery list  comp

---
+++ frontend/src/components/WordUser/Vocabulary/GalleryList/index.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import GalleryItem from '../GalleryItem';
+import PropTypes from 'prop-types';
+import Tooltip from '@mui/material/Tooltip';
+import useStyle from './style';
+
+const perPage = 7;
+function GalleryList({ list, total}) {
+  const classes = useStyle();
+  const [currentPage, setCurrentPage]= useState(1);
+
+  const handleNextClick = () => {
+    if (currentPage < total) {
+      setCurrentPage(currentPage +1);
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+     setCurrentPage(currentPage-1)
+    }
+  };
+
+  return (
+    <div className={`${classes.root} english-container`}>
+      {list && list.length > 0 &&(
+        <>
+          {/* gallery */}
+          {list.slice((currentPage-1)*perPage, currentPage*perPage).map((item, index) => (
+            <GalleryItem key={index} {...item}/>
+          ))}
+
+          {/* navigation arrow */}
+          {currentPage > 1 && (
+            <Tooltip title="Previous">
+              <span className="nav-arrow prev" onClick={handlePrevClick} />
+            </Tooltip>
+          )}
+          {currentPage < total && (
+            <Tooltip title="Next">
+              <span className="nav-arrow next" onClick={handleNextClick} />
+            </Tooltip>
+          )}
+        </>
+      //  ) : (
+      //   <>
+      //     {new Array(7).fill(0).map((_, index) => (
+      //       <div key={index}>
+      //         <Skeleton
+      //           className="w-100 h-100"
+      //           variant="rect"
+      //           animation="wave"
+      //         />
+      //       </div>
+      //     ))}
+        // </>
+      )}
+    </div>
+  );
+}
+
+GalleryList.propTypes = {
+  current: PropTypes.number,
+  list: PropTypes.array,
+  onNext: PropTypes.func,
+  onPrev: PropTypes.func,
+  total: PropTypes.number,
+};
+
+GalleryList.defaultProps = {
+  list: [],
+  onPrev: function () {},
+  onNext: function () {},
+  total: 1,
+  current: 1,
+};
+
+export default GalleryList;
