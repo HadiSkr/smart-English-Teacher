98f2c591c349e73355a6bb4ed1be1848296da977
ibra-kdbra <git-repo-develop@outlook.com>
2023-08-19 23:01:05 +0300

    IPA page

---
+++ frontend/src/pages/IPA.js
@@ -0,0 +1,45 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React from "react";
+import Consonants from "../components/IPA/Consonants";
+import Diphthongs from "../components/IPA/Diphthongs";
+import Vowels from "../components/IPA/Vowels";
+import useTitle from "../hooks/useTitle";
+import Typography from "@mui/material/Typography";
+import { dictionaryRoot } from "../components/UI/style";
+import { makeStyles } from "@mui/styles";
+
+const useStyle = makeStyles((theme) => ({
+  ...dictionaryRoot(theme),
+}));
+
+function IPAPage() {
+  useTitle("IPA");
+  const classes = useStyle();
+  return (
+    <div className={`${classes.root} english-container`}>
+      <Typography variant="h4" align="center" style={{ color: "#CA3600" }}>
+        Pronunciation
+      </Typography>
+      <Typography variant="h6" align="center">
+        Introduction to The Sounds of English
+      </Typography>
+
+      <p align="center">
+        <iframe
+          src="https://www.youtube.com/embed/fdRmGvmeY1U?enablejsapi=1"
+          width="500"
+          height="300"
+        ></iframe>
+      </p>
+      <Typography align="center">
+        This is the introductory video to our The sounds of English series
+      </Typography>
+
+      <Vowels />
+      <Diphthongs />
+      <Consonants />
+    </div>
+  );
+}
+
+export default IPAPage;
